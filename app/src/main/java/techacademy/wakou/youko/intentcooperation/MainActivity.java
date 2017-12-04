57a3a381ef2c9035e0546939d643a63a3aeee20f
youko-wakou <appu2pudding@gmail.com>
2017-12-04 16:48:09 +0900

    Initial commit

---
+++ app/src/main/java/techacademy/wakou/youko/intentcooperation/MainActivity.java
@@ -0,0 +1,31 @@
+package techacademy.wakou.youko.intentcooperation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText= (EditText)findViewById(R.id.editText);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent();
+                Intent.setAction(Intent.ACTION_SEND);
+                Intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, mEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
